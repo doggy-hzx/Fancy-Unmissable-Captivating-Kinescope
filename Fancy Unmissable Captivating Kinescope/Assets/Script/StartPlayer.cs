38f6dee99114b13fa90b86a55821e5e3a98f6520
doggy-hzx <2229662841@qq.com>
2021-04-01 19:27:07 +0800

    新建脚本文件夹

---
+++ Fancy Unmissable Captivating Kinescope/Assets/Script/StartPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        StartPlay();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void StartPlay() {
+        Debug.Log("Hello Unity!");
+    }
+
+}
